c1801fac2b1db3db87d5e1f5fab23d4a61df8eb4
Shresth Saxena <saxenashresth834@gmail.com>
2025-08-03 11:25:56 +0530

    Initial commit

---
+++ Questions/SwapInArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class SwapInArray{
+    public static void main(String[] args) {
+        int[] arr = {1,12,3,45,41};
+        swap(arr,1,3);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr,int index0,int index3){
+        int temp = arr[index0];
+        arr[index0] = arr[index3];
+        arr[index3] = temp;
+    }
+}
