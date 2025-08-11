38278a486765c6eae46e37caf4bf244c622e4d0a
Shresth Saxena <saxenashresth834@gmail.com>
2025-08-11 12:15:53 +0530

    intial commit

---
+++ Binary Search/PeakElementInMountainArray.java
@@ -0,0 +1,25 @@
+// leetcode 852
+import java.util.*;
+public class PeakElementInMountainArray{
+    public static void main(String[] nums){
+        int[] arr ={0,1,0};
+        int ans = PeakElement(arr);
+        System.out.println(ans);
+        
+    }
+    public static int PeakElement(int[] arr){
+        int st=0,end=arr.length-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1]){
+                return arr[mid];
+            }else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]){
+                st=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+}
+// time-complexcity =O(log(n)).
